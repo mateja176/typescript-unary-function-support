94a2b7914e388c6cf264d22a127fb1b37b1970f4
Mateja Petrovic <mateja176@gmail.com>
2019-03-16 10:45:22 +0100

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+const a = 1
+
+const f = (a: number) => a + 1
+const g = (a: number) => a * 2
+
+type Compose = <A, B, C>(f: (a: A) => B, g: (a: B) => C) => (a: A) => C
+const compose: Compose = (f, g) => a => g(f(a))
+
+const fg = compose(
+  f,
+  g,
+)
+
+const r = fg(a)
+
+type ComposeU = <A, B, C>(f: (a: A) => B) => (g: (a: B) => C) => (a: A) => C
+const composeU: ComposeU = f => g => a => g(f(a))
+
+const fgU = composeU(f)(g)
+
+const rU = fgU(a)
